2c46aabfb2e6450f23ec050d0598d4a5c0d924c3
OwenC03 <owen.carson@edu.sait.ca>
2024-03-29 20:12:30 -0600

    first push

---
+++ Recursion assignment/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Recursion_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition");
+            Console.WriteLine("Enter number n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int sum = RecursiveSum(n, m);
+            Console.WriteLine(sum);
+
+            Console.WriteLine("Division");
+            Console.WriteLine("Enter your number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int divisions = CountDivisions(num);
+            Console.WriteLine("Total number of divisions: " + divisions);
+        }
+
+        static int RecursiveSum(int n, int m)
+        {
+            if (n == m)
+            
+                return n;
+            
+            else
+            
+                return n + RecursiveSum(n + 1, m);
+            
+        }
+
+        static int CountDivisions(int num)
+        {
+            int count = 0;
+            while (num % 2 == 0)
+            {
+                num /= 2;
+                count++;
+            }
+            return count;
+        }
+    }
+}
